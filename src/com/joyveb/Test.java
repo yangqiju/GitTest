127c2e53eaf29832a559099bd03b10c8b7322613
yangqiju <362991493@qq.com>
2013-04-06 16:46:45 +0800

    test class

---
+++ src/com/joyveb/Test.java
@@ -0,0 +1,14 @@
+package com.joyveb;
+/**   
+ * @Title: Test.java 
+ * @Package com.joyveb 
+ * @Description: TODO(用一句话描述该文件做什么) 
+ * @author 杨其桔
+ * @date 2013-4-6 下午4:46:13 
+ * @version V1.0   
+ */
+public class Test {
+
+	public String tset="";
+}
+
